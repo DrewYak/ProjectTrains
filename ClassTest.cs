68cbb548dafe5f4f1041739de3d17bb1873230b8
ksusha rum <kseniyarum92@gmail.com>
2012-12-16 15:10:12 +0400

    Ksen_Last2

--- ClassTest.cs
+++ ClassTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Forms;
 using NUnit.Framework;
 
 namespace Trains
@@ -10,60 +11,101 @@ namespace Trains
         [Test]
         public void TestSearch()
         {
-            string FileName = @"E:\Trains\Project\Trains\TrainsAndAnotherStuff.xml";
-            Data.LoadFromFile(FileName);
-            
-            Passenger Psg0 = Passenger.Search(1000);
-            Assert.AreEqual("Румянцева", Psg0.LastName);
-            Assert.AreEqual("Ксения",    Psg0.FirstName);
-            Assert.AreEqual("Плацкарт",  Psg0.Tickets[0].Type);
-            Assert.AreEqual(10, Psg0.Tickets[0].Train.Number);
+            try
+            {
+                string FileName = @"C:\TRAINS\ProjectTrains\TrainsAndAnotherStuff.xml";
+                Data.LoadFromFile(FileName);
 
-            Passenger Psg1 = Passenger.Search(1001);
-            Assert.AreEqual("Трофименко", Psg1.LastName);
-            Assert.AreEqual("Дарья",      Psg1.FirstName);
-            Assert.AreEqual("Плацкарт",   Psg1.Tickets[0].Type);
-            Assert.AreEqual(10, Psg1.Tickets[0].Train.Number);
+                Passenger Psg0 = Passenger.Search(1000);
+                Assert.AreEqual("Румянцева", Psg0.LastName);
+                Assert.AreEqual("Ксения", Psg0.FirstName);
+                Assert.AreEqual("Плацкарт", Psg0.Tickets[0].Type);
+                Assert.AreEqual(10, Psg0.Tickets[0].Train.Number);
 
-            Passenger Psg2 = Passenger.Search(1002);
-            Assert.AreEqual("Алёшина",   Psg2.LastName);
-            Assert.AreEqual("Анастасия", Psg2.FirstName);
-            Assert.AreEqual("Плацкарт",  Psg2.Tickets[0].Type);
-            Assert.AreEqual(10, Psg2.Tickets[0].Train.Number);
+                Passenger ActPsg2 = Passenger.Search(2000);
+                Assert.AreEqual(null, ActPsg2);
+                                
 
-            Passenger Psg3 = Passenger.Search(1003);
-            Assert.AreEqual("Яковличев", Psg3.LastName);
-            Assert.AreEqual("Андрей",    Psg3.FirstName);
-            Assert.AreEqual("Плацкарт",  Psg3.Tickets[0].Type);
-            Assert.AreEqual(10, Psg3.Tickets[0].Train.Number);
-
-            Passenger ActPsg2 = Passenger.Search(2000);
-            Assert.AreEqual(null, ActPsg2);
-
-
-
-            
-                List<Passenger> FName0 = Passenger.Search("Румянцева", "", "");
-                Assert.AreEqual("Ксения", FName0[0].FirstName);
-                Assert.AreEqual("Плацкарт", FName0[0].Tickets[0].Type);
-                Assert.AreEqual(10, FName0[0].Tickets[0].Train.Number);
 
                 List<Passenger> FName1 = Passenger.Search("Трофименко", "", "");
                 Assert.AreEqual("Дарья", FName1[0].FirstName);
                 Assert.AreEqual("Плацкарт", FName1[0].Tickets[0].Type);
                 Assert.AreEqual(10, FName1[0].Tickets[0].Train.Number);
-            
+
+                List<Passenger> FName4 = Passenger.Search("Иванов", "", "");
+                Assert.IsEmpty(FName4);
 
 
-            List<Passenger> LPas = Passenger.Search("", "", "Плацкарт");
-            Assert.AreEqual(4,    LPas.Count);
-            Assert.AreEqual(1000, LPas[0].ID);
-            Assert.AreEqual(1001, LPas[1].ID);
-            Assert.AreEqual(1002, LPas[2].ID);
-            Assert.AreEqual(1003, LPas[3].ID);
 
-            List<Passenger> LPas1 = Passenger.Search("", "", "Купе");
-            Assert.IsEmpty(LPas1);
+                List<Passenger> LName2 = Passenger.Search("", "Анастасия", "");
+                Assert.AreEqual("Алёшина", LName2[0].LastName);
+                Assert.AreEqual("Плацкарт", LName2[0].Tickets[0].Type);
+                Assert.AreEqual(10, LName2[0].Tickets[0].Train.Number);
+
+                List<Passenger> LName4 = Passenger.Search("", "Иван", "");
+                Assert.IsEmpty(LName4);
+
+
+
+
+                List<Passenger> LPas = Passenger.Search("", "", "Плацкарт");
+                Assert.AreEqual(4, LPas.Count);
+                Assert.AreEqual(1000, LPas[0].ID);
+                Assert.AreEqual(1001, LPas[1].ID);
+                Assert.AreEqual(1002, LPas[2].ID);
+                Assert.AreEqual(1003, LPas[3].ID);
+
+                List<Passenger> LPas1 = Passenger.Search("", "", "Купе");
+                Assert.IsEmpty(LPas1);
+
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!");
+            }
         }
     }
+
+[TestFixture]
+    class TestTrn
+    {
+    [Test]
+    public void TestSearch1()
+    {
+       try
+       {
+            string FileName = @"C:\TRAINS\ProjectTrains\TrainsAndAnotherStuff.xml";
+            Data.LoadFromFile(FileName);
+
+            
+            Train Trn0 = Train.Search(10);
+            Assert.AreEqual(4, Trn0.CountOfPas);
+            Assert.AreEqual("Москва", Trn0.PointOfDeparture);
+            Assert.AreEqual("Санкт-Петербург", Trn0.PointOfArrival);
+
+
+            Train Trn1 = Train.Search(99);
+            Assert.AreEqual(null, Trn1);
+
+
+
+            List<Train> Stn1 = Train.Search("Москва","");
+            Assert.AreEqual("06:00", Stn1[0].TimeOfDeparture);
+            Assert.AreEqual(1000, Stn1[0].Tickets[0].Passenger.ID);
+
+            List<Train> Stn2 = Train.Search("", "Санкт-Петербург");
+            Assert.AreEqual("16:00", Stn2[0].TimeOfArrival);
+            Assert.AreEqual("Плацкарт", Stn1[0].Tickets[0].Type);
+
+            List<Train> Stn3 = Train.Search("Кострома", "");
+            Assert.IsEmpty(Stn3);
+
+
+        }
+        catch
+        {
+            MessageBox.Show("Ошибка!");
+        }
+    }
+  }
 }
